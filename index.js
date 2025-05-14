818ff8b41f7e6b173ccb95cc79a1e7f52f27a0e4
gonyango <grantononyango@gmail.com>
2025-05-14 10:37:37 +0300

    Refactor drawMatrix function to correctly apply offsets when rendering the matrix

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const matrix = [
   [0,1,0]
 ]
 
-matrix.forEach((row, y) => {
-  row.forEach((value, x) => {
-    if (value != 0){
-      context.fillStyle = 'red'
-      context.fillRect(x,y,1,1)
-    }
+function drawMatrix(matrix, offset){
+  matrix.forEach((row, y) => {
+    row.forEach((value, x) => {
+      if (value != 0){
+        context.fillStyle = 'red'
+        context.fillRect(x + offset.x,y + offset.y,1,1)
+      }
+    })
   })
-})
+}
+
+
+drawMatrix(matrix, {x : 5, y : 5})
