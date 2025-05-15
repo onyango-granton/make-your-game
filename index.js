73d1c08f4a07232f47e6e21a2a86e696ed19c91a
gonyango <grantononyango@gmail.com>
2025-05-15 07:22:58 +0300

    Implement arenaSweep function to clear completed rows and call it on player drop

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ const matrix = [
   [0,1,0]
 ]
 
+function arenaSweep(){
+  outer: for (let y = arena.length - 1; y > 0; --y){
+    for (let x = 0; x < arena[y].length; ++x){
+      if (arena[y][x] === 0) {
+        continue outer
+      }
+    }
+    const row = arena.splice(y, 1)[0].fill(0)
+    arena.unshift(row)
+    ++y
+  }
+}
+
 function collide(arena, player){
   const [m, o] = [player.matrix, player.pos]
 
@@ -179,6 +192,7 @@ function playerDrop(){
     player.pos.y--
     merge(arena, player)
     playerReset()
+    arenaSweep()
     //player.pos.y = 0
   }
   dropCounter = 0
